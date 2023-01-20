0e116927be0605bcefc12b014a9b3c4f9b94cc9a
Draper <27962761+Drapersniper@users.noreply.github.com>
2023-01-20 12:03:08 +0000

    log errors

--- audiohotfix/main.py
+++ audiohotfix/main.py
@@ -1,5 +1,5 @@
 import asyncio
-import contextlib
+from logging import getLogger
 
 from redbot.core.bot import Red
 from redbot.core.commands import commands
@@ -10,6 +10,7 @@ class AudioHotFix(commands.Cog):
 
     def __init__(self, bot: Red):
         self.bot = bot
+        self.logger = getLogger("red.3pt.audiohotfix.AudioHotFix")
         if version_info >= VersionInfo.from_str("3.5.0.dev294"):
             raise EnvironmentError("The fix which this cog implements is already in Red")
         if version_info < VersionInfo.from_str("3.5.0.dev0"):
@@ -19,48 +20,59 @@ class AudioHotFix(commands.Cog):
             self.attribute_name = "player_manager"
         else:
             self.attribute_name = "managed_node_controller"
-        self.loop = None
+        self.read_buffer_task = None
         self.monitor = asyncio.create_task(self.task_restart)
         self.cog_monitor_task = None
         self.buffer_exit = False
 
     async def loop_for_cog(self):
-        while not (__:= self.bot.get_cog("Audio")):
-            await asyncio.sleep(1)
-        if self.loop is not None:
-            self.loop = asyncio.create_task(self.read_buffer)
-        while (__:= self.bot.get_cog("Audio")):
-            await asyncio.sleep(1)
-            if self.buffer_exit is True:
-                if self.loop is not None:
-                    self.loop.cancel()
-                self.loop = asyncio.create_task(self.read_buffer)
-        self.loop = None
+        try:
+            while not (__:= self.bot.get_cog("Audio")):
+                await asyncio.sleep(1)
+            if self.read_buffer_task is not None:
+                self.read_buffer_task = asyncio.create_task(self.read_buffer)
+            while (__:= self.bot.get_cog("Audio")):
+                await asyncio.sleep(1)
+                if self.buffer_exit is True:
+                    if self.read_buffer_task is not None:
+                        self.read_buffer_task.cancel()
+                    self.read_buffer_task = asyncio.create_task(self.read_buffer)
+            self.read_buffer_task = None
+        except Exception as exc:
+            self.logger.info("Error in loop_for_cog", exc_info=exc)
 
 
     async def read_buffer(self):
-        self.buffer_exit = False
-        while True:
-            with contextlib.suppress(Exception):
-                cog = self.bot.get_cog("Audio")
-                attribute = getattr(cog, self.attribute_name, None)
-                if attribute is None or attribute._proc is None:
-                    self.buffer_exit = True
-                    return
-                async for __ in attribute._proc.stdout:
-                    pass
-        self.buffer_exit = True
+        try:
+            self.buffer_exit = False
+            while True:
+                try:
+                    cog = self.bot.get_cog("Audio")
+                    attribute = getattr(cog, self.attribute_name, None)
+                    if attribute is None or attribute._proc is None:
+                        self.buffer_exit = True
+                        return
+                    async for __ in attribute._proc.stdout:
+                        pass
+                except Exception as exc:
+                    self.logger.info(exec, exc_info=exc)
+            self.buffer_exit = True
+        except Exception as exc:
+            self.logger.info("Error in read_buffer", exc_info=exc)
 
     async def task_restart(self):
-        while True:
-            if self.cog_monitor_task is None:
-                await self.bot.wait_until_red_ready()
-                self.cog_monitor_task = asyncio.create_task(self.loop_for_cog)
-            await asyncio.sleep(1)
+        try:
+            while True:
+                if self.cog_monitor_task is None:
+                    await self.bot.wait_until_red_ready()
+                    self.cog_monitor_task = asyncio.create_task(self.loop_for_cog)
+                await asyncio.sleep(1)
+        except Exception as exc:
+            self.logger.info("Error in task_restart", exc_info=exc)
 
     def cog_unload(self):
         self.monitor.cancel()
-        if self.loop is not None and not self.loop.cancelled():
-            self.loop.cancel()
+        if self.read_buffer_task is not None and not self.read_buffer_task.cancelled():
+            self.read_buffer_task.cancel()
         if self.cog_monitor_task is not None and not self.cog_monitor_task.cancelled():
             self.cog_monitor_task.cancel()
