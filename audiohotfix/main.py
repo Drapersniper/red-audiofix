39fcbfbf8a1743005dd89b978759da04a7de40b9
Draper <27962761+Drapersniper@users.noreply.github.com>
2023-01-29 19:26:38 +0000

    fix

--- audiohotfix/main.py
+++ audiohotfix/main.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
 import asyncio
+import contextlib
 from logging import getLogger
 
+from redbot.cogs.audio import Audio
+from redbot.cogs.audio.manager import ServerManager
 from redbot.core.bot import Red
 from redbot.core.commands import commands
 from redbot import VersionInfo, version_info
@@ -20,18 +25,20 @@ class AudioHotFix(commands.Cog):
             self.attribute_name = "player_manager"
         else:
             self.attribute_name = "managed_node_controller"
-        self.read_buffer_task = None
         self.monitor = asyncio.create_task(self.task_restart())
+        self.read_buffer_task = None
         self.cog_monitor_task = None
         self.buffer_exit = False
 
     async def loop_for_cog(self):
         try:
-            while not (__:= self.bot.get_cog("Audio")):
+            while not (__ := self.bot.get_cog("Audio")):
                 await asyncio.sleep(1)
             if self.read_buffer_task is not None:
                 self.read_buffer_task = asyncio.create_task(self.read_buffer())
-            while (__:= self.bot.get_cog("Audio")):
+            while __ := self.bot.get_cog("Audio"):
+                if self.buffer_exit is False and self.read_buffer_task is None:
+                    self.read_buffer_task = asyncio.create_task(self.read_buffer())
                 await asyncio.sleep(1)
                 if self.buffer_exit is True:
                     if self.read_buffer_task is not None:
@@ -41,30 +48,32 @@ class AudioHotFix(commands.Cog):
         except Exception as exc:
             self.logger.info("Error in loop_for_cog", exc_info=exc)
 
-
     async def read_buffer(self):
         try:
             self.buffer_exit = False
             while True:
                 try:
-                    cog = self.bot.get_cog("Audio")
-                    attribute = getattr(cog, self.attribute_name, None)
+                    cog: Audio | None = self.bot.get_cog("Audio")
+                    attribute: ServerManager | None = getattr(cog, self.attribute_name, None)
                     if attribute is None or attribute._proc is None:
                         self.buffer_exit = True
                         return
-                    async for __ in attribute._proc.stdout:
-                        pass
+                    await attribute.ready.wait()
+                    with contextlib.suppress(RuntimeError):
+                        async for __ in attribute._proc.stdout:
+                            pass
+                    self.buffer_exit = True
+                    return
                 except Exception as exc:
-                    self.logger.info(exec, exc_info=exc)
-            self.buffer_exit = True
+                    self.logger.info(exc, exc_info=exc)
         except Exception as exc:
             self.logger.info("Error in read_buffer", exc_info=exc)
 
     async def task_restart(self):
         try:
+            await self.bot.wait_until_red_ready()
             while True:
                 if self.cog_monitor_task is None:
-                    await self.bot.wait_until_red_ready()
                     self.cog_monitor_task = asyncio.create_task(self.loop_for_cog())
                 await asyncio.sleep(1)
         except Exception as exc:
